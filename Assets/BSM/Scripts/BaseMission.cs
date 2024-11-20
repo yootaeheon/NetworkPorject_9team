7c62a22ec3dd9b97a8123b0dce147587cf123d3a
Vivid <suook15@naver.com>
2024-11-20 17:12:19 +0900

    [Create] BaseMission 스크립트 생성

---
+++ Assets/BSM/Scripts/BaseMission.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMission : MonoBehaviour
+{
+
+    private Dictionary<string, GameObject> _goDict;
+    private Dictionary<(string, System.Type), Component> _coDict;
+ 
+    protected void Bind()
+    {
+        Transform[] transforms = GetComponentsInChildren<Transform>(true);
+
+        _goDict = new Dictionary<string, GameObject>(transforms.Length << 2);
+
+        foreach(Transform child in transforms)
+        {
+            _goDict.TryAdd(child.gameObject.name, child.gameObject);
+        }
+
+        _coDict = new Dictionary<(string, System.Type), Component>(); 
+    }
+
+    protected GameObject GetObject(string name)
+    { 
+        _goDict.TryGetValue(name, out GameObject go); 
+        return go;
+    }
+
+    protected T GetComponent<T>(string name) where T : Component
+    {
+        (string, System.Type) _key = (name, typeof(T));
+
+        _coDict.TryGetValue(_key, out Component co);
+
+        if (co != null)
+            return co as T;
+
+        _goDict.TryGetValue(name, out GameObject go);
+
+        if(go != null)
+        {
+            co = go.GetComponent<T>();
+
+            if(co != null)
+            {
+                _coDict.TryAdd(_key, co);
+                return co as T;
+            } 
+        }
+
+        return null; 
+    }
+
+
+}
