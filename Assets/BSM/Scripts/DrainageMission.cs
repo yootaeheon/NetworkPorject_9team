770f3195bf4037def0b45a2345c34c776a13d52d
Vivid <suook15@naver.com>
2024-11-24 23:39:50 +0900

    [Create] 배수구 청소 미션 스크립트 생성

---
+++ Assets/BSM/Scripts/DrainageMission.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrainageMission : MonoBehaviour
+{
+    
+    //게임 시작 시 오브젝트들 List에 추가
+    //OnDisable 됐을 때 비활성화 된 오브젝트들 활성화 및 위치 초기화
+
+    //OnEnable
+        //미션 카운트 복구
+
+
+    //IF 마우스 좌 버튼 Down 
+        //오브젝트 마우스 좌표 따라다니기
+        //해당 오브젝트 좌표 기억
+        //오브젝트 이미지 살짝 축소
+
+    //ELSE IF 마우스 좌 버튼 Up
+        //IF 초기 위치에서 일정 거리만큼 이동 됐는지
+            //애니메이션 재생
+            //사운드 재생
+            //미션 카운트 감소
+            //오브젝트 Active False
+    
+        //ELSE
+            //오브젝트 마우스 따라가기 x
+            //오브젝트 크기 원상 복구
+
+
+
+}
