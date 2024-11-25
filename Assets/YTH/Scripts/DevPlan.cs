c31b6a298b5b50faccb8575501d692ff8291ac48
TaeHeon Ryoo <taroni55@naver.com>
2024-11-25 11:29:23 +0900

    [Create] 투표씬 의사 코드 작성

---
+++ Assets/YTH/Scripts/DevPlan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPlan : MonoBehaviour
+{
+    ///
+    /// 1. 투표에 필요한 데이터 생성 (VoteData stream 이용하여 변수 동기화 & 커스텀 프로퍼티 이용 각 플레이어의 투표 받은 수 동기화)
+    ///(커스텀 프로퍼티 : 각 플레이어 투표 받은 수, 그 투표받은 수 불러오기 생성 예정)
+    /// 
+    /// 2.투표 씬 입장 시 각 플레이어 패널들을 생성  PlayerNumbering을 이용해 플레이어 Panel 주인과 연동이 될 것임
+    /// 
+    /// 3. RPC 이용 상대 플레이어 패널을 눌르면 그 플레이어 패널 주인 (2번 과정 이용)에게 투표 받은 수 +1
+    /// 
+    /// 4. 투표 시간이 끝나면 (2번 과정 이용) 모든 customProperty.ContainsKey[] 이용하여 투표받은 수 집계 제일 득표수가 많은 플레이어 추방
+    ///
+
+
+
+    // VoteData 분리할것
+}
