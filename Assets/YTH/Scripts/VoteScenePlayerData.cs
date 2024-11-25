b7bfc0551673eff16f63c575c9bd7a7c73e7e17d
TaeHeon Ryoo <taroni55@naver.com>
2024-11-25 11:30:27 +0900

    [Refactor] VoteData 플레이어와 씬 데이터로 분리

---
+++ Assets/YTH/Scripts/VoteScenePlayerData.cs
@@ -0,0 +1,40 @@
+using Photon.Pun;
+using UnityEngine;
+
+[System.Serializable]
+public class VoteScenePlayerData : MonoBehaviourPun, IPunObservable
+{
+    [SerializeField] private int _voteCount; // 득표 수
+    public int VoteCount { get { return _voteCount; } set { _voteCount = value; } }
+
+
+    [SerializeField] private bool _didVote; // 투표했는지 여부
+    public bool DidVote { get { return _didVote; } set { _didVote = value; } }
+
+
+    [SerializeField] private bool _isDead; // 죽었는지 여부
+    public bool IsDead { get { return _isDead; } set { _isDead = value; } }
+
+
+    [SerializeField] private bool _isReporter; // 신고자인지 여부
+    public bool IsReporter { get { return _isReporter; } set { _isReporter = value; } }
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(VoteCount);
+            stream.SendNext(DidVote);
+            stream.SendNext(IsDead);
+            stream.SendNext(IsReporter);
+        }
+        else if (stream.IsReading)
+        {
+            VoteCount = (int)stream.ReceiveNext();
+            DidVote = (bool)stream.ReceiveNext();
+            IsDead = (bool)stream.ReceiveNext();
+            IsReporter = (bool)stream.ReceiveNext();
+        }
+    }
+}
