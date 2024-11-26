31582328031ac4c2db10026953d74d507accac56
TaeHeon Ryoo <taroni55@naver.com>
2024-11-26 12:06:04 +0900

    [Refactor] 사용하지 않는 변수 삭제 및 인스펙터 상 위치 수정 및 데이터 구조 조정

--- Assets/YTH/Scripts/VoteScenePlayerData.cs
+++ Assets/YTH/Scripts/VoteScenePlayerData.cs
@@ -5,10 +5,8 @@ using UnityEngine.UI;
 [System.Serializable]
 public class VoteScenePlayerData : MonoBehaviourPun, IPunObservable
 {
-    [SerializeField] public Button voteButton;
-
-    [SerializeField] private int _voteCount; // 득표 수
-    public int VoteCount { get { return _voteCount; } set { _voteCount = value; } }
+    [SerializeField] public Button _voteButton;
+    public Button VoteButton { get { return _voteButton; }  set { _voteButton = value; } }
 
 
     [SerializeField] private bool _didVote; // 투표했는지 여부
@@ -19,21 +17,19 @@ public class VoteScenePlayerData : MonoBehaviourPun, IPunObservable
     public bool IsDead { get { return _isDead; } set { _isDead = value; } }
 
 
-    [SerializeField] private bool _isReporter; // 신고자인지 여부
+    [SerializeField] private bool _isReporter; // 신고자 여부
     public bool IsReporter { get { return _isReporter; } set { _isReporter = value; } }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(VoteCount);
             stream.SendNext(DidVote);
             stream.SendNext(IsDead);
             stream.SendNext(IsReporter);
         }
         else if (stream.IsReading)
         {
-            VoteCount = (int)stream.ReceiveNext();
             DidVote = (bool)stream.ReceiveNext();
             IsDead = (bool)stream.ReceiveNext();
             IsReporter = (bool)stream.ReceiveNext();
