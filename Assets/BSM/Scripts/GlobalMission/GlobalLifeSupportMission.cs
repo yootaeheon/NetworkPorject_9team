9720b57e7cfe006a7ab25317141b4e263d0102b1
Vivid <suook15@naver.com>
2024-11-28 23:04:05 +0900

    [Create] Global 생명유지 장치 미션 스크립트 생성

---
+++ Assets/BSM/Scripts/GlobalMission/GlobalLifeSupportMission.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLifeSupportMission : MonoBehaviour
+{
+    //왼쪽 슬롯 리스트
+    //왼쪽 칩 리스트
+    
+    //오른쪽 슬롯 리스트
+
+    //상, 하 대기 슬롯 리스트
+    //오른쪽 슬롯에 넣을 칩 리스트
+
+    
+    //칩들이 어떤 타입인지 가지고 있고, 그 타입을 이용해서 오른쪽 슬롯이랑 리스트 비교해가면 될듯
+
+    //OnEnable
+        //왼쪽 슬롯에 있는 칩들 리스트에 집어넣고
+        //순서 셔플
+        //그리고 첫 번째부터 Position 배치
+        
+    //각 슬롯 1 ~ 9 번째 칸 마다 Position을 잡아줄 빈 게임 오브젝트 배치
+       
+    //오른쪽 슬롯
+        //각 칩들 클릭 후 드래그하면 그 슬롯에 배치
+        //배치가 다 끝났을 경우 왼쪽 슬롯과 0번째부터 8번째 까지 타입 비교
+        
+    //모든 요소의 타입이 같으면 미션 Clear
+    //하나라도 다를 경우 Fail
+
+
+
+}
