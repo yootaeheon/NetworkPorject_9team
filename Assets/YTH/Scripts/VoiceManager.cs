c8dba1cab2f8634d500db1eed65d36457a93d03c
TaeHeon Ryoo <taroni55@naver.com>
2024-11-28 16:56:47 +0900

    [Feat] 게임 씬 보이스 적용

--- Assets/YTH/Scripts/VoiceManager.cs
+++ Assets/YTH/Scripts/VoiceManager.cs
@@ -16,7 +16,7 @@ public class VoiceManager : MonoBehaviourPunCallbacks
 
     [SerializeField] Photon.Voice.Unity.Recorder _recorder;
 
-    [SerializeField] GameObject _image;
+    //[SerializeField] GameObject _image;
    // private PhotonVoiceView _voiceView;
 
     private Speaker _speaker;
@@ -71,14 +71,14 @@ public class VoiceManager : MonoBehaviourPunCallbacks
 
         
         
-        if (_recorder.IsCurrentlyTransmitting) // 포톤보이스뷰.isSpeaking 으로 수정해야함 각가 플레이어가 포톤보이스뷰 들고있어야할듯
-        {
-            _image.SetActive(true);
-        }
-        else
-        {
-            _image.SetActive(false);
-        }
+       // if (_recorder.IsCurrentlyTransmitting) // 포톤보이스뷰.isSpeaking 으로 수정해야함 각가 플레이어가 포톤보이스뷰 들고있어야할듯
+       // {
+       //     _image.SetActive(true);
+       // }
+       // else
+       // {
+       //     _image.SetActive(false);
+       // }
         
     }
 
