7bb2e2fe384a50f46f83be6a87d05189f9721f2c
Vivid <suook15@naver.com>
2024-11-30 22:06:08 +0900

    [Feat] 사보타지 팔 컬러 값 연동 기능 추가

--- Assets/BSM/Scripts/AbilityColor.cs
+++ Assets/BSM/Scripts/AbilityColor.cs
@@ -5,17 +5,12 @@ using UnityEngine.UI;
 
 public class AbilityColor : MonoBehaviour
 {
-
-    private MissionState _state;
     private MissionController _controller;
 
 
     private void Start()
     {
         _controller = GetComponent<MissionController>();
-        _state = GetComponent<MissionState>();
-
-        _controller.GetMissionObj<Image>("Arm").color = MissionState.PlayerColor;
-
+        _controller.GetMissionObj<Image>("Arm").color = MissionState.PlayerColor; 
     }
 }
