6156b8e38a5bdbf4833f76c81dd903676ee5706d
TaeHeon Ryoo <taroni55@naver.com>
2024-12-01 23:21:59 +0900

    [Fix] 생존 / 사망 플레이어 보이스 분리

---
+++ Assets/YTH/Scripts/SeparationVoice.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Voice.Unity;
+using UnityEngine;
+
+public class SeparationVoice : MonoBehaviour
+{
+    PlayerDataContainer _playerDataContainer => PlayerDataContainer.Instance;
+
+    private Speaker _speaker;
+
+    private void Awake()
+    {
+        Speaker speaker = GetComponent<Speaker>();
+        _speaker = speaker;
+    }
+
+    private void Update()
+    {
+        // 플레이어 사망 시 스피커 위치 변경하여 보이스 분리
+        if (_playerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost)
+        {
+            _speaker.transform.position = new Vector3(0, 0, _speaker.transform.position.z - 500);
+        }
+    }
+}
+
--- Assets/YTH/Scripts/VoiceManager.cs
+++ Assets/YTH/Scripts/VoiceManager.cs
@@ -20,9 +20,6 @@ public class VoiceManager : MonoBehaviourPunCallbacks
 
     private Speaker _speaker;
 
-    private const byte LIVING_GROUP = 1;
-    private const byte DEAD_GROUP = 2;
-   
     private void Awake()
     {
         if (Instance == null)
@@ -34,75 +31,11 @@ public class VoiceManager : MonoBehaviourPunCallbacks
         {
             Destroy(gameObject);
         }
-
-        //TODO : 플레이어컨트롤러 겟컴포넌트로 참조 시킬 것
     }
-
-  //  void Start()
-  //  {
-  //      if (!PhotonNetwork.IsConnected)
-  //      {
-  //          PhotonNetwork.ConnectUsingSettings();
-  //      }
-  //  }
-
-    private void Update()
-    {
-        //IsSpeakingImageEnable();
-       // FindAndLogConnectedSpeakers();
-
-
-       // Invoke("IsSpeakingImageEnable", 3f);
-
-        // if (Input.GetKeyDown(KeyCode.Q))
-        // {
-        //     SetVoiceChannel(true);
-        // }
-        // else if (Input.GetKeyDown(KeyCode.W))
-        // {
-        //     SetVoiceChannel(false);
-        // }
-    }
-
-    // public override void OnConnectedToMaster()
-    // {
-    //     if (PhotonNetwork.IsConnectedAndReady && !PhotonNetwork.InLobby)
-    //     {
-    //         PhotonNetwork.JoinLobby();
-    //     }
-    // }
-
-    //  public override void OnJoinedLobby()
-    //  {
-    //      Util.GetDelay(3f);
-    //      Debug.Log("방 입장 중  ` ` ` ");
-    //      PhotonNetwork.JoinRoom(RoomName);
-    //  }
-
 
     public override void OnJoinedRoom()
     {
         Debug.Log("룸에 입장했습니다. Voice 설정을 확인합니다.");
-    }
-
-
-    // 플레이어 컨트롤에서 호출할 것
-    public void SetVoiceChannel(bool isGhost)
-    {
-        // IsGhohst 변경 시 이벤트로 하는게 좋을듯
-        // 살아있음 => 그룹 1 사용
-        if (isGhost)
-        {
-            _recorder.InterestGroup = LIVING_GROUP;
-            _recorder.TransmitEnabled = false;
-            Debug.Log("살아있음 : 1번 채널 이용중");
-        }
-        // 죽은 플레이어 => 그룹 2번 사용
-        else
-        {
-            _recorder.InterestGroup = DEAD_GROUP;
-            Debug.Log("사망하여 채널 전환 : 2번 채널");
-        }
     }
 
 
