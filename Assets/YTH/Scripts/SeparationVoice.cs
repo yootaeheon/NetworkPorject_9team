b236df8404f04253d2c6cc7f13bb0da1ad220ace
TaeHeon Ryoo <taroni55@naver.com>
2024-12-02 00:20:33 +0900

    [Fix] 스피커 인스턴스 생성 되기 전 호출 버그 수정

--- Assets/YTH/Scripts/SeparationVoice.cs
+++ Assets/YTH/Scripts/SeparationVoice.cs
@@ -19,11 +19,8 @@ public class SeparationVoice : MonoBehaviour
 
     private void Update()
     {
-        if (_playerDataContainer == null)
+        if (_playerDataContainer == null || LobbyScene.Instance != null || _speaker == null)
             return;
-
-        if (LobbyScene.Instance != null)
-            return ;
 
         // 플레이어 사망 시 스피커 위치 변경하여 보이스 분리
         if (_playerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost)
