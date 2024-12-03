0dc7685af9cdd2590571b31fb70f9ecfd1f49069
TaeHeon Ryoo <taroni55@naver.com>
2024-12-03 18:01:10 +0900

    [Fix] 인스턴스 미생성시 리턴 조건문 추가

--- Assets/YTH/Scripts/VoiceManager.cs
+++ Assets/YTH/Scripts/VoiceManager.cs
@@ -22,7 +22,7 @@ public class VoiceManager : MonoBehaviourPunCallbacks
     private void Update()
     {
         StartCoroutine(SetSpeaker());
-        StartCoroutine(SetTargetPlayer());
+        StartCoroutine(SetTargetPlayerRoutine());
     }
 
     public void DisableVoice()
@@ -49,44 +49,60 @@ public class VoiceManager : MonoBehaviourPunCallbacks
         }
     }
 
-    IEnumerator SetTargetPlayer()
+    IEnumerator SetTargetPlayerRoutine()
     {
-        yield return 1f.GetDelay();
 
-        // 액터넘버 == 타겟플레이어 배열에이 있는 인덱스
-        // 타겟플레이어 배열에 인덱스 번호로 보이스를 보냄
-
-        // 플레이어데이터를 코루틴으로 계속 받음
-        // 죽은 사람 있으면 인덱스 번호 삭제
-
-        // 본인 플레이어 생존 상황
-        if (PlayerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost == false)
+        while (true)
         {
-            // 사망 플레이어 인덱스 삭제
-            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            if (PlayerDataContainer == null)
             {
-                if (PlayerDataContainer.GetPlayerData(i).IsGhost == true)
-                {
-                    _recorder.TargetPlayers[i] = 0;
-                }
+                yield return null;
             }
-        }
-
-
-        // 본인 플레이어 사망 상황
-        if (PlayerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost == true)
-        {
-            // 모든 인덱스 0으로 초기화 + 사망 플레이어 인덱스 추가
-            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            else 
             {
-                _recorder.TargetPlayers[i] = 0;
-            }
-
-            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
-            {
-                if (PlayerDataContainer.GetPlayerData(i).IsGhost == true)
+                while (true)
                 {
-                    _recorder.TargetPlayers[i] = i + 1;
+                    yield return 1f.GetDelay();
+                    if (PlayerDataContainer == null)
+                        break;
+
+                    // 액터넘버 == 타겟플레이어 배열에이 있는 인덱스
+                    // 타겟플레이어 배열에 인덱스 번호로 보이스를 보냄
+
+                    // 플레이어데이터를 코루틴으로 계속 받음
+                    // 죽은 사람 있으면 인덱스 번호 삭제
+
+                    // 본인 플레이어 생존 상황
+                    if (PlayerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost == false)
+                    {
+                        // 사망 플레이어 인덱스 삭제
+                        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+                        {
+                            if (PlayerDataContainer.GetPlayerData(i).IsGhost == true)
+                            {
+                                _recorder.TargetPlayers[i] = 0;
+                            }
+                        }
+                    }
+
+
+                    // 본인 플레이어 사망 상황
+                    if (PlayerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost == true)
+                    {
+                        // 모든 인덱스 0으로 초기화 + 사망 플레이어 인덱스 추가
+                        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+                        {
+                            _recorder.TargetPlayers[i] = 0;
+                        }
+
+                        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+                        {
+                            if (PlayerDataContainer.GetPlayerData(i).IsGhost == true)
+                            {
+                                _recorder.TargetPlayers[i] = i + 1;
+                            }
+                        }
+                    }
                 }
             }
         }
