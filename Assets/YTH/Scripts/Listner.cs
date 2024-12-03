9a472abde76026a64e9d88217ca3d58597f3c06d
TaeHeon Ryoo <taroni55@naver.com>
2024-12-03 16:00:06 +0900

    [Feat] 사보타지 오브젝트 수정

--- Assets/YTH/Scripts/Listner.cs
+++ Assets/YTH/Scripts/Listner.cs
@@ -1,17 +1,18 @@
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Listner : MonoBehaviourPun
 {
     private GameLoadingScene _scene;
 
-   [SerializeField] Vector3 offset;
+    [SerializeField] Vector3 offset;
 
     private void Update()
     {
+        if (GameLoadingScene.MyPlayer == null)
+            return;
+
         if (PlayerDataContainer.Instance.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost)
         {
             gameObject.transform.position = GameLoadingScene.MyPlayer.transform.position + offset;
