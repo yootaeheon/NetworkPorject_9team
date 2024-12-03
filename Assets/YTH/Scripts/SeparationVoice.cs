767d74d712ab5f2b64ffaf0e966642b3c06dd8eb
TaeHeon Ryoo <taroni55@naver.com>
2024-12-03 10:54:57 +0900

    [Fix] 거리에 따른 소리 조절 문제 수정중

--- Assets/YTH/Scripts/SeparationVoice.cs
+++ Assets/YTH/Scripts/SeparationVoice.cs
@@ -14,42 +14,49 @@ public class SeparationVoice : MonoBehaviourPun
     {
         yield return null;
         Speaker speaker = GetComponentInChildren<Speaker>();
-     //   speaker.transform.SetParent(null);
+        //   speaker.transform.SetParent(null);
         _speaker = speaker;
+
+        StartCoroutine(SeparateVoice());
     }
 
     private void Update()
     {
-        if (PlayerDataContainer == null || LobbyScene.Instance != null || _speaker == null)
-            return;
-        SeparateVoice();
-
+      
 
         // 게임중
-      //  if (VoteScene.Instance == null)
-      //  {
-      //      _speaker.gameObject.SetActive(true);
-      //  }
-      //  // 투표중
-      //  else
-      //  {
-      //      _speaker.gameObject.SetActive(false);
-      //  }
+        //  if (VoteScene.Instance == null)
+        //  {
+        //      _speaker.gameObject.SetActive(true);
+        //  }
+        //  // 투표중
+        //  else
+        //  {
+        //      _speaker.gameObject.SetActive(false);
+        //  }
     }
 
-    public void SeparateVoice()
+    public IEnumerator SeparateVoice()
     {
-        photonView.RPC(nameof(SeparateVoiceRpc), RpcTarget.AllBuffered);
+        while (true)
+        {
+            if (PlayerDataContainer == null || LobbyScene.Instance != null || _speaker == null)
+            {
+                yield return null;
+            }
+            else
+            {
+                yield return 0.5f.GetDelay();
+                photonView.RPC(nameof(SeparateVoiceRpc), RpcTarget.AllBuffered);
+            }
+        }
+
     }
 
     // 플레이어 사망 시 스피커 위치 변경하여 보이스 분리
-
     [PunRPC]
     public void SeparateVoiceRpc()
     {
-        if (photonView.IsMine == false)
-            return;
-
         if (PlayerDataContainer.GetPlayerData(PhotonNetwork.LocalPlayer.GetPlayerNumber()).IsGhost)
         {
             _speaker.transform.position = new Vector3(0, 0, 30);
